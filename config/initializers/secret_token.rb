645d87beecdd4577d03165c33d9293c48f7b9271
david.small <david.small@industrieit.com>
2015-07-21 16:00:47 +1000

    create project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Burbon::Application.config.secret_token = 'c1bf3d130f54175f57a5739770b1edbb56bafeac23c103008dfc27a34872f3db4003eaf152be1080ae8fdb0bf87cb9fd4e6f1eaa3d2af3d98064d0c2bd355c57'
